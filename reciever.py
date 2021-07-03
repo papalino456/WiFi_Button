b79f52c5acbac620924e4bca018a95b5828a8580
papalino456 <sebastian.barrio.b@gmail.com>
2021-07-02 20:22:55 -0500

    finish template reciever code (just turning on a GPIO pin)

--- reciever.py
+++ reciever.py
@@ -1,7 +1,10 @@
 import usocket
+import machine
 
 s = usocket.socket()
 MYespIP="192.168.100.98"
+
+actuator = machine.Pin(2,machine.Pin.OUT)
 
 s.bind((MYespIP, 1234))
 s.listen(5)
@@ -10,7 +13,11 @@ print("el servidor esta escuchando en 192.168.100.98: 1234")
 while True:
     clientSocket, address = s.accept()
     print("connection from {} has been established".format(address))
-    msg = clientSocket.recv(1024)
-    print(msg.decode("utf-8"))
-    clientSocket.send(bytes("Hola desde el server!", "utf-8"))
+    msg = clientSocket.recv(1024).decode("utf-8")
+    print("el valor recibido fue: ", msg)
+    clientSocket.send(bytes("Valor recivido!", "utf-8"))
     clientSocket.close()
+    if msg == 1:
+        actuator.on()
+    if msg == 0:
+        actuator.off()

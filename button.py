a541908e6ce82874750871a6ca27ff3ec526362a
papalino456 <sebastian.barrio.b@gmail.com>
2021-07-02 20:18:44 -0500

    finish button client

--- button.py
+++ button.py
@@ -1,11 +1,11 @@
 import usocket
-import urandom
-import subprocess
+button = machine.Pin(2, machine.Pin.IN)
 while True:
-    n = urandom.randint(1, 10000)
-    s = usocket.socket()
-    s.connect(("192.168.100.98", 1234))
-    s.send(bytes("n={}".format(n), "utf-8"))
-    res = s.recv(1024)
-    print(res.decode("utf-8"))
-    s.close()
+    if button.value == 1:
+        n = 1
+        s = usocket.socket()
+        s.connect(("192.168.100.98", 1234))
+        s.send(bytes("n={}".format(n), "utf-8"))
+        res = s.recv(1024)
+        print(res.decode("utf-8"))
+        s.close()
